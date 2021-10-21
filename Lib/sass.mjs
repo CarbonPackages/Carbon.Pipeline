7b042330916098f93f1b1df2ad40da860ccc03c0
Jon Uhlmann <me@jonnitto.ch>
2021-10-21 11:11:47 +0200

    Update: Pass all kind of sass options to compiler (#14)

--- Lib/sass.mjs
+++ Lib/sass.mjs
@@ -1,17 +1,17 @@
 import sass from "sass";
 import tildeImporter from "node-sass-tilde-importer";
-import { styleFiles, compression } from "./helper.mjs";
+import { config, styleFiles, compression } from "./helper.mjs";
 function render(key) {
     const { to, sourcemap } = styleFiles[key];
     const result = sass.renderSync({
-        file: key,
-        outFile: to[0],
         outputStyle: compression ? "compressed" : "expanded",
-        importer: tildeImporter,
         sourceMap: sourcemap,
         sourceMapContents: true,
         sourceMapEmbed: true,
-        quietDeps: quietDeps
+        ...config.sassOptions,
+        file: key,
+        outFile: to[0],
+        importer: tildeImporter,
     });
     const css = result.css.toString();
     const importedFiles = [...result.stats.includedFiles].filter((file) => file != key);
