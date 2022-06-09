5d2ac1b90d8a751873e407ab67e72270840d2f94
Jon Uhlmann <account@uhlmann.pro>
2022-06-09 14:55:01 +0200

    Update: Add types for tailwind config

--- Installer/Distribution/Defaults/tailwind.config.js
+++ Installer/Distribution/Defaults/tailwind.config.js
@@ -1,5 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
 const content = require("./Build/Carbon.Pipeline/purge");
 
 module.exports = {
     content,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
